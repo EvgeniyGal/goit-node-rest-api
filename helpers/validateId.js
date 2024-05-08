9111fb9264665d10fa9761c436818d3713809b12
Evgeniy <egeniygal@gmail.com>
2024-05-08 21:22:30 +0300

    Fix validateId

--- helpers/validateId.js
+++ helpers/validateId.js
@@ -1,5 +1,5 @@
 import HttpError from "./HttpError.js";
-import { ObjectId } from "mongodb";
+import ObjectId from "mongoose";
 
 const validateId = (req, _, next) => {
   const isValidObjectId = ObjectId.isValidObjectId(req.params.id);
